3f7e5e74ec3863b13a44ecbed6acf3428a5d9c23
Neko65535 <tigerincatfamily@gmail.com>
2026-04-30 12:03:35 +0800

    2026/4/30.4-1

---
+++ Home/data.js
@@ -0,0 +1,41 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "門口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.2553193769163684,
+          "pitch": -0.5730462761297765,
+          "title": "<span style=\"font-size: 13px; background-color: rgba(58, 68, 84, 0.8);\">洸影水晶燈 - 展示間</span>",
+          "text": "水晶燈工廠直營專賣店<div><br>地址：新北市蘆洲區<div>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;仁愛街77巷12號1 樓</div><div>電話：(02)2847-3975</div></div>"
+        }
+      ]
+    }
+  ],
+  "name": "Home",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
